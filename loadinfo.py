ef0f9763c6cf0a1bffa8d421163a2fc101c3f900
ondelia <noobsensei@hotmail.com>
2021-05-30 17:08:35 -0400

    Added default environmental variables.

--- loadinfo.py
+++ loadinfo.py
@@ -12,6 +12,22 @@ def loadinfo():
     qb_login = os.getenv('QB_LOGIN')
     qb_password = os.getenv('QB_PASSWORD')
 
+    # Use defaults if no value specified
+    if not movie_list:
+        movie_list = "Movie List.xlsx"
+    if not destination:
+        destination = "C:\Movies"
+        try:
+            os.mkdir(destination)
+        except:
+            pass
+    if not qb_client:
+        qb_client = "http://127.0.0.1:8080/"
+    if not qb_login:
+        qb_login = "admin"
+    if not qb_password:
+        qb_password = "adminadmin"
+
     # Load download source
     t = TPB()
 
